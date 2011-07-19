f15ec7f3fa5156213482d93c91e3e317d3a8a872
Trevor <trevorknight@gmail.com>
2011-07-18 20:15:38 -0400

    Changes to layouts

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,7 +5,9 @@ class Artist < ActiveRecord::Base
   validates :name, :presence => true,
                    :uniqueness => true
   
+  default_scope order('artists.name')
   scope :where_name, lambda {|term| where("artists.name LIKE ?", "%#{term}%") }
+  
   
   def owned_by?(owner)
     return false unless owner.is_a? User
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -6,7 +6,9 @@ class Venue < ActiveRecord::Base
                    :uniqueness => true
   validates :address, :presence => true
   
+  default_scope order('venues.name')
   scope :where_name, lambda {|term| where("venues.name LIKE ?", "%#{term}%") }
+  scope :recently_updated, lambda {where("venues.updated_at > ?", 5.days.ago) }
 				   
   def owned_by?(owner)
     return false unless owner.is_a? User
