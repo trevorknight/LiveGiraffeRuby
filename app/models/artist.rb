8b17c6a272d9a03c57966e7c23f975be1aa81d47
David Weigl <david@livegiraffe.com>
2011-07-20 15:57:57 -0400

    Added before_create to artist model, to fetch canonical name from EchoNest

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,8 @@
+require 'nokogiri'
+require 'open-uri'
+require 'uri'
+echoKey = 'TTXOSQ9K9L1WDCRFA'
+
 class Artist < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :events
@@ -8,7 +13,12 @@ class Artist < ActiveRecord::Base
   default_scope order('artists.name')
   scope :where_name, lambda {|term| where("artists.name LIKE ?", "%#{term}%") }
   
-  
+  def before_create
+      #retrieve canonical artist name from echonest
+      echonest = Nokogiri::HTML(open("http://developer.echonest.com/api/v4/artist/search?api_key=#{echoKey}&name=#{URI.escape(name)}&format=xml"))
+      name = echonest.css('name').first.inner_html
+  end
+
   def owned_by?(owner)
     return false unless owner.is_a? User
     user == owner
