aee8b3a89d4467c53af60588d86e657e741d3aff
Trevor <trevorknight@gmail.com>
2011-07-27 10:55:32 -0400

    Moved All to Calendar button to view in order to fix, need to move back to helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,5 @@
 module EventsHelper
-
+  # This needs to be fixed: the id variable passed works to perform the check but doesn't work to find the event. Need to investigate variable scope.
   def add_or_remove_event_button(id)
     if logged_in?
       if !current_user.profile.events.member?(Event.find(id))
@@ -8,6 +8,8 @@ module EventsHelper
           f.submit t('views.events.add_to_calendar') 
         end
       end
+    else
+      t('views.events.login_for_calendar')
     end
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :events
-  before_create :canonicalise_name
+  before_create :canonicalise_name 
     
   validates :name, :presence => true,
                    :uniqueness => true
